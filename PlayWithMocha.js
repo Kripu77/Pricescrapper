34fd99f816db35a71bd366296344834bb29bdf1c
Kripu <kripu.12345@gmail.com>
2022-04-16 09:38:20 +1000

    Init files

---
+++ PlayWithMocha.js
@@ -0,0 +1,10 @@
+//specify what the test is going to do
+
+describe('MCD PRICE SCRAPPER', function (){
+
+    //test case
+it('should click on the login button', function(){
+console.log("I am ready")
+})
+
+})
