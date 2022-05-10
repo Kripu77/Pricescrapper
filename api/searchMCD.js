de88af7d151a980a479ed3cab142ee709587b447
Kripu <kripu.12345@gmail.com>
2022-05-10 20:38:34 +1000

    refactor of timestamp

--- api/searchMCD.js
+++ api/searchMCD.js
@@ -123,7 +123,7 @@ console.log(compliedPrice)
 })
   it('should write the data', async()=>{
    compliedPrice.map((value)=>{
-    fs.writeFile(`./results/${searchData[0].storeName}_Product_Extract_${timeStamp}.csv`, `${value.productName}, ${value.price}\n`, {flag:'a'}, (err, result)=>{
+    fs.writeFile(`./results/${searchData[0].storeName}_Product_Extract.csv`, `${value.productName}, ${value.price}\n`, {flag:'a'}, (err, result)=>{
       if(err) throw err;
           })
       
